a4abfe43954ba05d337f324750952633838d6bc3
firashaj <fhadj92@gmail.com>
2021-12-12 12:32:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import firas from './code.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = {
+    person: {
+      fullName: 'firas haj',
+      bio: 'love coding',
+      imgSrc: firas,
+      profession: 'ui/ux designer',
+    },
+    show: false,
+    count: 0,
+  }
+  HandleShow = () => {
+    this.setState({
+      show: !this.state.show,
+    })
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Hello</h1>
+        {this.state.show && (
+          <>
+            <h2>fullname : {this.state.person.fullName}</h2>
+            <h2>bio : {this.state.person.bio}</h2>
+            <img className='img' src={this.state.person.imgSrc} />
+            <h2>profession:{this.state.person.profession}</h2>
+            <h2>{this.state.count}</h2>
+          </>
+        )
+        }
+
+        <button  class="btn btn-outline-success" onClick={this.HandleShow} >Show</button>
+        
+
+
+
+      </div>
+    )
+  }
 }
-
 export default App;
